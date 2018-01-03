c81974048ed18df003ab96f253551ec644db1635
mrpapercut <mischa.rodermond@gmail.com>
2018-01-03 22:58:55 +0100

    added user.html

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +42,15 @@ public class SignupController {
         signupRepository.save(new Signup(name, street, postcode, city, photo));
         return "done";
     }
-
+    
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public String userPage(Model model, @PathVariable Long id) {
+        // Get a user from the signupRepository
+        
+        Signup user = signupRepository.getOne(id);
+        
+        model.addAttribute("username", user.getName());
+        
+        return "user";
+    }
 }
